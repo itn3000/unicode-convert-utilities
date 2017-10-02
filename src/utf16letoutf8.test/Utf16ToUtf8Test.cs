a29ce1ebfbd42fb4ee7dfedee4ac9116fd736929
itn3000 <itn3000@gmail.com>
2017-10-03 00:22:29 +0900

    Utf16Utility.GetUtf8Bytes pass test

--- src/utf16letoutf8.test/Utf16ToUtf8Test.cs
+++ src/utf16letoutf8.test/Utf16ToUtf8Test.cs
@@ -8,6 +8,42 @@ namespace utf16letoutf8.test
     public class Utf16ToUtf8Test
     {
         [Fact]
+        public void Utf8BytesU7F()
+        {
+            var expected = new char[] { (char)0x01, (char)0x7f, (char)0x12 };
+            var bytes = Utf16Utility.GetUtf8Bytes(new string(expected));
+            var actual = Encoding.UTF8.GetString(bytes.Array, bytes.Offset, bytes.Count).ToCharArray();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Utf8BytesU7FF()
+        {
+            var expected = new char[] { (char)0x80, (char)0x7ff, (char)0x123 };
+            var bytes = Utf16Utility.GetUtf8Bytes(new string(expected));
+            var actual = Encoding.UTF8.GetString(bytes.Array, bytes.Offset, bytes.Count).ToCharArray();
+            Assert.Equal(expected.Select(x => (int)x), actual.Select(x => (int)x));
+        }
+        [Fact]
+        public void Utf8BytesUFFFF()
+        {
+            var expected = new char[] { (char)0x800, (char)0xd7ff, (char)0xffff };
+            var bytes = Utf16Utility.GetUtf8Bytes(new string(expected));
+            var actual = Encoding.UTF8.GetString(bytes.Array, bytes.Offset, bytes.Count).ToCharArray();
+            Assert.Equal(expected.Select(x => (int)x), actual.Select(x => (int)x));
+        }
+        [Fact]
+        public void Utf8BytesU10FFFF()
+        {
+            var expected = new char[]{
+                (char)0xd800, (char)0xdc00,
+                (char)0xdbff, (char)0xdfff,
+                (char)0xd802, (char)0xdc52,
+            };
+            var bytes = Utf16Utility.GetUtf8Bytes(new string(expected));
+            var actual = Encoding.UTF8.GetString(bytes.Array, bytes.Offset, bytes.Count).ToCharArray();
+            Assert.Equal(expected.Select(x => (int)x), actual.Select(x => (int)x));
+        }
+        // [Fact]
         public void AsciiString()
         {
             var str = "abcde012345=!";
--- src/utf16letoutf8/Utf16ToUtf8.cs
+++ src/utf16letoutf8/Utf16ToUtf8.cs
@@ -2,8 +2,120 @@
 
 namespace utf16letoutf8
 {
+    using System.Runtime.CompilerServices;
     public static class Utf16Utility
     {
+        public static unsafe ArraySegment<byte> GetUtf8Bytes(string source)
+        {
+            var retbuf = new byte[source.Length * 3];
+            fixed (char* srcptr = source)
+            {
+                unchecked
+                {
+                    char* endptr = srcptr + source.Length;
+                    char* iterptr = srcptr;
+                    byte* retptr = (byte*)Unsafe.AsPointer(ref retbuf[0]);
+                    byte* beginptr = retptr;
+                    while (iterptr < endptr)
+                    {
+                        UpdateCharUnsafe(ref retptr, ref iterptr, ref endptr);
+                    }
+                    return new ArraySegment<byte>(retbuf, 0, (int)(retptr - beginptr));
+                }
+            }
+        }
+        static unsafe void UpdateCharUnsafe(ref byte* retptr, ref char* iterptr, ref char* endptr)
+        {
+            if (*iterptr < 0x80)
+            {
+                // U+7F
+                *retptr = (byte)*iterptr;
+                retptr++;
+                iterptr++;
+            }
+            else if (*iterptr < 0x800)
+            {
+                // U+7FF
+                // 
+                *retptr = (byte)(
+                    // 3bit
+                    0xc0 
+                    // 5bit
+                    | ((*iterptr & 0x7c0) >> 6)
+                );
+                retptr[1] = (byte)(
+                    // 2bit
+                    0x80 
+                    // 6bit
+                    | ((*iterptr & 0x3f))
+                );
+                retptr += 2;
+                iterptr++;
+            }
+            else if ((*iterptr & 0xfc00) == 0xd800)
+            {
+                if(iterptr + 1 >= endptr || (iterptr[1] & 0xfc00) != 0xdc00)
+                {
+                    throw new InvalidOperationException("invalid utf-16 sequence(missing surrogate pair)");
+                }
+                // surrogate pair(U+10FFFF)
+                var u = ((0x03c0 & *iterptr) >> 6) + 1;
+                *retptr = (byte)(
+                    // 5bit
+                    0xf0 
+                    // 3bit
+                    | (u >> 2)
+                );
+                retptr[1] = (byte)(
+                    // 2bit 
+                    0x80
+                    // 2bit 
+                    | ((u & 0x3) << 4)
+                    // 4bit
+                    | ((*iterptr & 0x3c) >> 2)
+                );
+                retptr[2] = (byte)(
+                    // 2bit
+                    0x80
+                    // 2bit
+                    | ((*iterptr & 0x3) << 4)
+                    // 4bit
+                    | (((iterptr[1] & 0x3c0) >> 6))
+                );
+                retptr[3] = (byte)(
+                    // 2bit
+                    0x80
+                    // 6bit
+                    | ((iterptr[1] & 0x3f))
+                );
+                retptr += 4;
+                iterptr += 2;
+            }
+            else
+            {
+                // U+FFFF
+                *retptr = (byte)(
+                    // 4bit
+                    0xe0
+                    // 4bit
+                    | ((*iterptr & 0xf000) >> 12)
+                );
+                retptr[1] = (byte)(
+                    // 2bit
+                    0x80
+                    // 6bit
+                    | ((*iterptr & 0xfc0) >> 6)
+                );
+                retptr[2] = (byte)(
+                    // 2bit
+                    0x80
+                    // 6bit
+                    | ((*iterptr & 0x3f))
+                );
+                retptr += 3;
+                iterptr++;
+            }
+        }
         public static Utf8Enumerator GetEnumerator(char[] ar, int offset, int count)
         {
             return new Utf8Enumerator(ar, offset, count);
@@ -17,9 +129,7 @@ namespace utf16letoutf8
             byte second;
             byte third;
             byte forth;
-            byte fifth;
-            byte sixth;
-            int currentCharPosition;
+            sbyte currentCharPosition;
             public Utf8Enumerator(char[] ar, int offset, int count)
             {
                 charArray = ar;
@@ -29,8 +139,6 @@ namespace utf16letoutf8
                 second = 0;
                 third = 0;
                 forth = 0;
-                fifth = 0;
-                sixth = 0;
                 currentCharPosition = -1;
             }
             public bool TryGetNext(out byte b)
@@ -49,12 +157,6 @@ namespace utf16letoutf8
                     case 3:
                         b = forth;
                         break;
-                    case 4:
-                        b = fifth;
-                        break;
-                    case 5:
-                        b = sixth;
-                        break;
                     default:
                         b = 0;
                         break;
@@ -69,14 +171,18 @@ namespace utf16letoutf8
                     if ((c & 0xD800) == 0xD800)
                     {
                         // surrogate
-                        var c2 = charArray[currentIndex+1];
+                        var c2 = charArray[currentIndex + 1];
 
-                    }else{
-                        if(c < 0x80)
+                    }
+                    else
+                    {
+                        if (c < 0x80)
                         {
                             first = (byte)c;
-                        }else if(c < 0x800){
-                            
+                        }
+                        else if (c < 0x800)
+                        {
+
                         }
                     }
                 }
